13fdf6c1f9183135888d1129945cfab94cebb7dc
Adrian Mysliwiec <mysliwiec.a02@htlwienwest.at>
2021-11-18 13:52:56 +0100

    config

---
+++ vue.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  pwa: {
+    manifestOptions: {
+      name: 'Simple2Train',
+      short_name: 'Simple2Train',
+      theme_color: '#58BC53',
+      background_color: '#00262A',
+      icons: [
+        {
+          src: './img/icons/android-chrome-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+        },
+        {
+          src: './img/icons/android-chrome-192x192.png',
+          sizes: '192x192',
+          type: 'image/png',
+        },
+      ],
+    },
+  },
+};
